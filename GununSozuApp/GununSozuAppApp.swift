f5f0932e95c219a2b3fe084022c043c8e2e6db9f
Başak Altuntaş <basakaltuntas@gmail.com>
2023-08-15 16:20:44 +0300

    Initial Commit

---
+++ GununSozuApp/GununSozuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GununSozuAppApp.swift
+//  GununSozuApp
+//
+//  Created by Başak on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct GununSozuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
